fb162211a691ed98be522eb30ec63c9532ff26e0
dgrib <gds194@bk.ru>
2020-11-25 23:12:08 +0500

    task 6 added

---
+++ lesson-5-6.py
@@ -0,0 +1,19 @@
+"""6. Необходимо создать (не программно) текстовый файл,
+где каждая строка описывает учебный предмет и наличие лекционных,
+практических и лабораторных занятий по этому предмету и их количество.
+Важно, чтобы для каждого предмета не обязательно были все типы занятий.
+Сформировать словарь, содержащий название предмета и общее количество занятий по нему.
+Вывести словарь на экран. {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}"""
+subject_dict = {}
+with open('dz_5_6.txt', 'r', encoding='utf-8') as f_obj:
+    for subj_line in f_obj:
+        subj_list = subj_line.split()
+        hours_sum = 0
+        for lesson_type_index in range(1, len(subj_list)):  # бежим по элементам: 100(л) 50(пр) 20(лаб)
+            lesson_type_hours = "0"
+            for i in subj_list[lesson_type_index]:  # бежим по элементу: 100(л), выцепляем число
+                if i.isdigit():
+                    lesson_type_hours += i
+            hours_sum += int(lesson_type_hours)
+        subject_dict[subj_list[0][:-1]] = hours_sum
+print(subject_dict)
