d299180573066178b69b96705fa172db5eca7d23
dgrib <gds194@bk.ru>
2020-11-25 22:14:46 +0500

    task 5 added

--- lesson-5-5.py
+++ lesson-5-5.py
@@ -1,2 +1,10 @@
 """5. Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами.
 Программа должна подсчитывать сумму чисел в файле и выводить ее на экран."""
+from functools import reduce
+with open('dz_5_5.txt', 'w', encoding='utf-8') as f_obj:
+    f_obj.write(str(' '.join([str(i) for i in range(20) if i % 2 == 0])))
+
+with open('dz_5_5.txt', 'r', encoding='utf-8') as f_obj:
+    line = f_obj.read().split()
+    print(f"Numbers in file:\n{line}\n")
+    print(f"Sum of numbers in file: {sum(list(map(int, line)))}")
