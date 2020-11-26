3ab7c2058ba5cf8bf4ffa5c57a40c7b55b624f5f
gribanov_dmitrii <gds1984@bk.ru>
2020-11-26 17:18:01 +0500

    task 7 added

--- lesson-5-7.py
+++ lesson-5-7.py
@@ -6,5 +6,21 @@
 Пример списка: [{“firm_1”: 5000, “firm_2”: 3000, “firm_3”: 1000}, {“average_profit”: 2000}].
 Итоговый список сохранить в виде json-объекта в соответствующий файл.
 """
+import json
+
+firms_profit = {}
+average_profit = []  # для накопления прибылей фирм без убытков
+answer_list = []
 with open('dz_5_7.txt', 'r', encoding='utf-8') as f_obj:
     for subj_line in f_obj:
+        firm_data_list = subj_line.split()
+        profit = int(firm_data_list[2]) - int(firm_data_list[3])
+        firms_profit[firm_data_list[0]] = abs(profit)  # заполняем словарь {фирма: прибыль/убыток}
+        if profit > 0:
+            average_profit.append(profit)
+
+answer_list.append(firms_profit)
+answer_list.append({'average_profit': int(sum(average_profit) / len(average_profit))})
+
+with open("dz_5_7.json", "w") as write_f:
+    json.dump(answer_list, write_f)
