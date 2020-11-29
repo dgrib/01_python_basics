d7d7ba18da43516ecb0fe1abebc570a3c711ac57
dgrib <gds194@bk.ru>
2020-11-29 22:37:24 +0500

    task 5 added

--- lesson-6-5.py
+++ lesson-6-5.py
@@ -5,3 +5,41 @@
 Для каждого из классов методы должен выводить уникальное сообщение.
 Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 """
+
+
+class Stationery:
+
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print('Запуск отрисовки.')
+
+
+class Pen(Stationery):
+    def draw(self):
+        print(f'Запуск отрисовки {self.title}.')
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print(f'Запуск отрисовки {self.title}.')
+
+
+class Handle(Stationery):
+    def draw(self):
+        print(f'Запуск отрисовки {self.title}.')
+
+
+pen_1 = Pen('Ручка_1')
+pen_2 = Pen('Ручка_2')
+pen_1.draw()
+pen_2.draw()
+pencil_3 = Pencil('Карандаш_4')
+pencil_3.draw()
+pencil_4 = Pencil('Карандаш_4')
+pencil_4.draw()
+handle_5 = Handle('Ручка_5')
+handle_5.draw()
+handle_6 = Handle('Ручка_6')
+handle_6.draw()
