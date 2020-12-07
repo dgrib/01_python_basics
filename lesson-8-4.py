0c05255b55cda0de9fb672360713e85a9b410790
gribanov_dmitrii <gds1984@bk.ru>
2020-12-07 18:18:54 +0500

    4 task changed

--- lesson-8-4.py
+++ lesson-8-4.py
@@ -19,11 +19,16 @@ from abc import abstractmethod
 
 
 class Warehouse:
-    acceptance = {'printer': [], 'scanner': [], 'laptop': []}
-    project_team = {'printer': [], 'scanner': [], 'laptop': []}
-    sales_team = {'printer': [], 'scanner': [], 'laptop': []}
+    warehouse = {'warehouse_printers': [], 'warehouse_scanners': [], 'warehouse_laptops': []}
+    accountants = {'printer': [], 'scanner': [], 'laptop': []}
+    programmers = {'printer': [], 'scanner': [], 'laptop': []}
 
-    # def to_project_team(self):
+    @staticmethod
+    def get_warehouse():
+        print("Содержимое склада: ")
+        for key, value in Warehouse.warehouse.items():
+            print('\033[91m', key, ':', '\033[0m')
+            print(value, '\n')
 
 
 class OfficeEquip:
@@ -32,7 +37,6 @@ class OfficeEquip:
     def __init__(self, model):
         self.model = model
         OfficeEquip.id += 1
-        self.type = ''
 
     # @abstractmethod
     def make_acceptance(self):
@@ -43,13 +47,12 @@ class Printer(OfficeEquip):
     def __init__(self, model, mfu=False):
         super().__init__(model)
         self.mfu = mfu
-        self.type = 'printer'
 
     def __str__(self):
         return f'{self.id} {self.model} {self.mfu}'
 
     def make_acceptance(self):
-        Warehouse.acceptance['printer'].append([self.id, self.type, self.model, self.mfu])
+        Warehouse.warehouse['warehouse_printers'].append([self.id, self.model, self.mfu])
 
     def to_some_office(self):
         pass
@@ -74,6 +77,31 @@ class Printer(OfficeEquip):
 
 
 printer_1 = Printer('Canon 6600')
+printer_2 = Printer('Canon 250')
 print(printer_1)
 printer_1.make_acceptance()
-print(Warehouse.acceptance)
+
+print(Warehouse.warehouse)
+
+while True:
+    print('1 - вывод информации\n2 - ввод оборудования на склад\nquit - для выхода\n')
+    user_key = input("Введите номер операции: ").lower()
+    if user_key == 'quit':
+        break
+    elif user_key == '1':
+        Warehouse.get_warehouse()
+        # for key, value in Warehouse.warehouse.items():
+        #     print('\033[91m', key, ':', '\033[0m')
+        #     print(value, '\n')
+    elif user_key == '2':
+        user_key = input('1 - ввод принтера\n2 - ввод сканера\n3 - ввод ноутбука\nquit - выход\nВведите номер '
+                         'операции: ')
+        if user_key == 'quit':
+            print()
+            continue
+        elif user_key == '1':
+            name = input('Введите модель принтера: ')
+            if_mfu = input('Наличие функции МФУ: (+ / - (по умолчанию)): ')
+            printer_1 = Printer(name, True if if_mfu == '+' else False)
+            printer_1.make_acceptance()
+            Warehouse.get_warehouse()
